cef8f2e7e8675be05cba69f04b49b5cf351294f5
32BITAcademy <32bitacademy@gmail.com>
2022-01-08 18:34:41 +0300

    Завершена основная часть калькулятора

--- OOP_Git/Source.cpp
+++ OOP_Git/Source.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include"Miron.h"
+#include"Misha.h"
+#include"Vanya.h"
 
 using namespace std;
 
@@ -12,7 +15,9 @@ int main()
 	int res;
 	switch (c)
 	{
-		;
+	case '+': res = add(a, b); break;
+	case '-': res = sub(a,b); break;
+	case '*': res = multiply(a,b); break;
 	}
 
 	cout << "Result = " << res << endl;
